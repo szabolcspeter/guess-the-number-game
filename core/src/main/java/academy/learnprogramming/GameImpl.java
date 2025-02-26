5f53a9123cf7fbd4c9b0ff1486132ec2c666a7d7
Szabolcs Peter <szabi.peter@gmail.com>
2025-02-26 10:39:26 +0100

    Autowiring beans with field injection

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -2,6 +2,7 @@ package academy.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -10,6 +11,7 @@ public class GameImpl implements Game {
 
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -33,10 +35,6 @@ public class GameImpl implements Game {
     @PreDestroy
     public void preDestroy() {
         log.info("in Game preDestroy()");
-    }
-
-    public GameImpl(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
     }
 
     @Override
