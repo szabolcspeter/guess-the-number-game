f670b09530b32b4fb013415cf342d83816d54968
Szabolcs Peter <szabi.peter@gmail.com>
2025-04-14 06:54:02 +0200

    Constructor-based dependency injection

--- core/src/main/java/szabolcspeter/learnprogramming/Main.java
+++ core/src/main/java/szabolcspeter/learnprogramming/Main.java
@@ -18,6 +18,7 @@ public class Main {
         ConfigurableApplicationContext context
                 = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
 
+        // get number generator bean from context (container)
         NumberGenerator numberGenerator
                 = context.getBean("numberGenerator", NumberGenerator.class);
 
@@ -26,6 +27,12 @@ public class Main {
 
         // log generated number
         log.info("number = {}", number);
+
+        // get number generator bean from context (container)
+        Game game = context.getBean(Game.class);
+
+        // call reset method()
+        game.reset();
 
         // close context (container)
         context.close();
