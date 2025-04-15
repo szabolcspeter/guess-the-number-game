91b2b11d05f2b9936bba164e2fb812d6282c57ed
Szabolcs Peter <szabi.peter@gmail.com>
2025-04-15 14:55:56 +0200

    Instance field (numberGenerator) injection with Autowiring

--- core/src/main/java/szabolcspeter/learnprogramming/GameImpl.java
+++ core/src/main/java/szabolcspeter/learnprogramming/GameImpl.java
@@ -2,6 +2,7 @@ package szabolcspeter.learnprogramming;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -10,6 +11,7 @@ public class GameImpl implements Game {
 
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
+    @Autowired
     private NumberGenerator numberGenerator;
     private int guessCount = 10;
     private int number;
@@ -18,14 +20,6 @@ public class GameImpl implements Game {
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
-
-    /*public GameImpl(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }*/
-
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
 
     // == init ==
     @PostConstruct
