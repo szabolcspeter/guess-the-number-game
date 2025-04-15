fbaf243cf470eff16fd4f95a148d16a82957842a
Szabolcs Peter <szabi.peter@gmail.com>
2025-04-15 15:44:18 +0200

    Annotation Config implemented

---
+++ core/src/main/java/szabolcspeter/learnprogramming/AppConfig.java
@@ -0,0 +1,21 @@
+package szabolcspeter.learnprogramming;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "szabolcspeter.learnprogramming")
+public class AppConfig {
+
+    // == bean methods == note: useful when additional bean config is needed
+    @Bean
+    public NumberGenerator numberGenerator() {
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game() {
+        return new GameImpl();
+    }
+}
--- core/src/main/java/szabolcspeter/learnprogramming/Main.java
+++ core/src/main/java/szabolcspeter/learnprogramming/Main.java
@@ -3,20 +3,18 @@ package szabolcspeter.learnprogramming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
-
-    private static final String CONFIG_LOCATION = "beans.xml";
 
     public static void main(String[] args) {
         log.info("Guess The Number Game");
 
         // create context (container)
         ConfigurableApplicationContext context
-                = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+                = new AnnotationConfigApplicationContext(AppConfig.class);
 
         // get number generator bean from context (container)
         NumberGenerator numberGenerator
