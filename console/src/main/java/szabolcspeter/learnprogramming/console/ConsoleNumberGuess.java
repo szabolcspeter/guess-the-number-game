0a527a20d6b99aecf1d26370b9e2d589f60d0f62
Szabolcs Peter <szabi.peter@gmail.com>
2025-04-20 10:00:57 +0200

    Application Events and Listener 2st way (annotation a)

--- console/src/main/java/szabolcspeter/learnprogramming/console/ConsoleNumberGuess.java
+++ console/src/main/java/szabolcspeter/learnprogramming/console/ConsoleNumberGuess.java
@@ -2,22 +2,17 @@ package szabolcspeter.learnprogramming.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
 
     public static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        log.info("Container ready for use.");
-    }
-
-    @Override
-    public boolean supportsAsyncExecution() {
-        return ApplicationListener.super.supportsAsyncExecution();
+    @EventListener
+    public void start(ContextRefreshedEvent event) {
+        log.info("start() => Container ready for use.");
     }
 }
